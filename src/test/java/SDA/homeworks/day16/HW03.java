1c8355e8c6708c42c26b4bab86b17d87e3c0ce32
nbindaej <n.bindaej@gmail.com>
2024-03-14 13:59:03 +0300

    day16

--- src/test/java/SDA/homeworks/day16/HW03.java
+++ src/test/java/SDA/homeworks/day16/HW03.java
@@ -11,21 +11,22 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-
-public class HW02 extends TestBase {
+public class HW03 extends TestBase {
     By username = By.id("username");
     By password = By.id("password");
     By submit = By.id("submit");
+    By error = By.id("error");
     @Test
     @Parameters({"username","password"})
-    public void positiveTest(String name,String pass){
+    public void negativeTest(String name,String pass){
         driver.get("https://practicetestautomation.com/practice-test-login/");
         driver.findElement(username).sendKeys(name);
         driver.findElement(password).sendKeys(pass);
         driver.findElement(submit).click();
 
-        String url = driver.getCurrentUrl();
-        Assert.assertTrue(url.contains("practicetestautomation.com/logged-in-successfully/"));
+        WebElement errorMessage = driver.findElement(error);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOf(errorMessage));
+        Assert.assertTrue(errorMessage.isDisplayed());
     }
-
 }
